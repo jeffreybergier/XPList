7e1300cea7c9587808f02f6efd781327d473e862
jeffreybergier <jeffburg@jeffburg.com>
2021-01-22 22:19:40 +0900

    added context menu - but not added to list yet

---
+++ Sources/XPList/ContextMenu.swift
@@ -0,0 +1,48 @@
+//
+//  Created by Jeffrey Bergier on 2021/01/22.
+//
+//  Copyright © 2020 Saturday Apps.
+//
+//  This file is part of XPList.
+//
+//  Hipstapaper is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Hipstapaper is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Hipstapaper.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+import SwiftUI
+
+extension XPL {
+    public struct ContextMenu<Element: Hashable, Menu: View>: ViewModifier {
+        
+        public typealias Selection = Set<Element>
+        public typealias Builder = (Selection) -> Menu
+        
+        private let builder: Builder?
+        private let selection: Selection
+        
+        public init(_ selection: Selection,
+                    _ builder: Builder?)
+        {
+            self.selection = selection
+            self.builder = builder
+        }
+        
+        public func body(content: Content) -> AnyView {
+            guard let builder = self.builder else { return AnyView(content) }
+            return AnyView(
+                content
+                    .contextMenu(menuItems: { builder(self.selection) })
+            )
+        }
+    }
+}
