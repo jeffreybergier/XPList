0575b3385b196f00103cc29be5e82fd1680d9184
jeffreybergier <jeffburg@jeffburg.com>
2021-02-13 20:14:48 +0900

    improved animation for edit mode

--- Sources/XPList/List1/Accessory.swift
+++ Sources/XPList/List1/Accessory.swift
@@ -32,14 +32,19 @@ extension XPL1 {
         @Environment(\.XPL_isEditMode) private var isEditMode
         @Environment(\.XPL_Configuration) private var config
         internal var body: some View {
-            guard self.isEditMode else { return AnyView(EmptyView()) }
             let image = self.isSelected
                 ? self.config.selectedAccessory
                 : self.config.deselectedAccessory
-            return AnyView(
-                image
-                    .foregroundColor(self.config.accessoryAccent)
-            )
+            image
+                .foregroundColor(self.config.accessoryAccent)
+                .frame(width: self.width, alignment: .trailing)
+                .opacity(self.opacity)
+        }
+        private var width: CGFloat? {
+            self.isEditMode ? nil : 0
+        }
+        private var opacity: Double {
+            self.isEditMode ? 1 : 0
         }
     }
 }
